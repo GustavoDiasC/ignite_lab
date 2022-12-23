2e6cb3250af31be799b51a8705f77d49a7734cf6
GustavoDiasC <kouenvidaloka2gmail.com>
2022-12-23 09:10:11 -0300

    add action deploy

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, {configType}) => {
     if (configType === 'PRODUCTION') {
-      config.base= '/GustavoDiasC/ignite_lab'
+      config.base = '/GustavoDiasC/ignite_lab'
     }
 
     return config
